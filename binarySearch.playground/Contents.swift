39abd6a24e577456c953861c30648c1572c22ddc
Paulo H.M <paulo.machtura@vee.digital>
2024-07-10 21:42:07 -0300

    Added playground file

---
+++ binarySearch.playground/Contents.swift
@@ -0,0 +1,21 @@
+class BinarySearch {
+    func search(in array: [Int], value: Int) -> Int{
+        var leftLimit = 0
+        var rightLimit = array.count - 1
+
+        while leftLimit < rightLimit {
+            var pivot = leftLimit + ( rightLimit - leftLimit)
+ 
+            if array[pivot] == value {
+                return pivot
+            }else if array[pivot] < value {
+                leftLimit = pivot + 1
+            }else if array[pivot] > value {
+                rightLimit = pivot - 1
+            }
+        }
+        return -1
+    }
+}
+
+print(BinarySearch().search(in: [0,2,4,5,6,7], value: 3))
